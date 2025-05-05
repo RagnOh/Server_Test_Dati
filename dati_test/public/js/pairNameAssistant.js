1794add75fe50b1a64df402aa559525fa19ae401
RagnOh <ragnolimichele99@gmail.com>
2025-05-05 12:39:35 +0200

    bootstrap

--- dati_test/public/js/pairNameAssistant.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("input[type=text]").keyup(function(){
-      $.ajax("/ajaxPairAssistant",{data: {nome: $(this).val()}, success: function(result) {
-        $("#risultati").html(result);
-      }});
-    });
-  });
